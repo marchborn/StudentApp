cb65f4876e540f19cb721e0ad8616eae345da73e
MUHAMMED SALIH K <muhdsalih13@gmail.com>
2024-02-22 17:13:38 +0530

    first commit

--- src/components/Addstudent.jsx
+++ src/components/Addstudent.jsx
@@ -1,5 +1,6 @@
-import { Button, TextField } from '@mui/material'
-import React, { useState } from 'react'
+import { Button, TextField, Typography } from '@mui/material'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const Addstudent = () => {
     var[data,setdata] = useState({})
@@ -15,11 +16,26 @@ const Addstudent = () => {
         
         setfname(data);
         console.log(fname)
+        axios.post("http://localhost:4000/students",{
+          name:fname.name,
+          age:fname.age,
+          department:fname.department
+        })
+        .then(()=>{
+          alert("New entry created successfully")
+        })
+        .catch(()=>{
+          alert("Error saving data")
+        })
     }
+    useEffect(() => {
+      console.log(fname)
+  }, [fname])
 
   return (
     <div>
         <br /><br />
+        <Typography style={{fontSize:'30px'}}>Student Details</Typography><br /><br />
         <TextField label='Name' variant='outlined' name='name' onChange={inputHandler}></TextField><br /><br />
         <TextField label='Age' variant='outlined' name='age' onChange={inputHandler}></TextField><br /><br />
         <TextField label='Department' variant='outlined' name='department' onChange={inputHandler}></TextField><br /><br />
--- src/components/Viewstudent.jsx
+++ src/components/Viewstudent.jsx
@@ -1,29 +1,48 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
-import React from 'react'
+import { Table,TableHead, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Viewstudent = () => {
+  const [student, setstudent] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/students')
+      .then((response) => {
+        console.log(response);
+        setstudent(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
-       <TableContainer>
+      <TableContainer>
         <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell style={{color:'red',fontFamily:'sans-serif',fontSize:'30px'}}>Name</TableCell>
-                    <TableCell style={{color:'red',fontFamily:'sans-serif',fontSize:'30px'}}>Age</TableCell>
-                    <TableCell style={{color:'red',fontFamily:'sans-serif',fontSize:'30px'}}>Department</TableCell>
-
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ color: 'red', fontFamily: 'sans-serif', fontSize: '30px' }}>Name</TableCell>
+              <TableCell style={{ color: 'red', fontFamily: 'sans-serif', fontSize: '30px' }}>Age</TableCell>
+              <TableCell style={{ color: 'red', fontFamily: 'sans-serif', fontSize: '30px' }}>Department</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {student.map((val, i) => {
+              return (
+                <TableRow key={i}>
+                  <TableCell>{val.name}</TableCell>
+                  <TableCell>{val.age}</TableCell>
+                  <TableCell>{val.department}</TableCell>
                 </TableRow>
-            </TableHead>
-            <TableBody>
-               
-            </TableBody>
+              );
+            })}
+          </TableBody>
         </Table>
-        
-        
-        
-        </TableContainer> 
+      </TableContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Viewstudent
+export default Viewstudent;
